920657694d8fc4805a1dc050d37e2d80bbbe7785
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-18 01:04:20 -0400

    created function to generate a random character from a given character set

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+/*Character set options*/
+
 var lowerChars = "abcdefghijklmnopqrstuvwxyz";
 var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChars = "123456789";
 var specialChars = "!@#$%^&*()_-+=<>?";
+
+function getRandomCharacter(characterSet) {
+  var randomIndex = Math.floor(Math.random() * characterSet.length);
+  return characterSet.charAt(randomIndex);
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
