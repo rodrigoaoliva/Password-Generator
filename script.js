18b7189bd61ab42f6ce46b7326abcd348ccdc0e7
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-18 01:24:50 -0400

    create function to generate a password based on user criteria

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChars = "123456789";
 var specialChars = "!@#$%^&*()_-+=<>?";
 
+// Function to generate a random character from a given character set
 function getRandomCharacter(characterSet) {
   var randomIndex = Math.floor(Math.random() * characterSet.length);
   return characterSet.charAt(randomIndex);
 }
+
+// Function to generate a password based on user criteria
+function generatePassword() {
+  var password = "";
+  var allowedChars = "";
+
+  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):");
+  passwordLength = parseInt(passwordLength); //converts strings to integers
+
+  if (passwordLength < 8) {
+  alert ("Please enter a password with more than 7 characters");
+  return ""
+  }
+
+  if (passwordLength > 128) {
+    alert ("Please enter a password with less than 129 characters");
+    return ""
+  }
+}
+
+
 
 
 
