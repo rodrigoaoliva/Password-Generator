cb3718ca1af5d5f6eb0f358ea010103f3ade45e3
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-20 18:04:06 -0400

    added back function to generate a password using the generatePassword function and set the generated password as the value of the HTML input element

--- script.js
+++ script.js
@@ -56,3 +56,15 @@ for (var i = 0; i < passwordLength; i++) {
 return password
 
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
