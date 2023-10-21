d22853683c2bbedd48912b4b74e1f70f6142a12a
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-21 13:38:31 -0400

    began adding descriptions to the javascript

--- script.js
+++ script.js
@@ -3,34 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 /*Character set options*/
 
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "123456789";
-var specialChars = "!@#$%^&*()_-+=<>?";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz"; // Defines lowercase characters
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Defines uppercase characters
+var numericChars = "123456789"; //Defines numeric characters
+var specialChars = "!@#$%^&*()_-+=<>?"; //Define special characters
 
 // Function to generate a random character from a given character set
-function getRandomCharacter(characterSet) {
-  var randomIndex = Math.floor(Math.random() * characterSet.length);
-  return characterSet.charAt(randomIndex);
+function getRandomCharacter(characterSet) { 
+  var randomIndex = Math.floor(Math.random() * characterSet.length); // Generates a random index within the range of the character set
+  return characterSet.charAt(randomIndex); //Returns the random character
 }
 
 // Function to generate a password based on user criteria
 function generatePassword() {
-  var password = "";
-  var allowedChars = "";
+  var password = ""; //Defines an empty starting value for the password
+  var allowedChars = ""; //Defines an empty starting string of allowed characters
 
-  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):");
-  passwordLength = parseInt(passwordLength); //converts strings to integers
+  var passwordLength = prompt("Enter the password length (between 8 and 128 characters):"); //Prompt for user to select password length
+  passwordLength = parseInt(passwordLength); //Converts strings to integers
 
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a valid password length.");
-    return "";
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {  //Condition to check if user selected a valid password length
+    alert("Please enter a valid password length."); //Gives the user an alert if password length is invalid
+    return ""; //Returns an empty password length
   }
 
-var includeLowercase = confirm("Include lowercase characters?");
-var includeUppercase = confirm("Include uppercase characters?");
-var includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
+var includeLowercase = confirm("Include lowercase characters?"); //Confirmation prompt asking the user to include lowercase characters
+var includeUppercase = confirm("Include uppercase characters?"); //Confirmation prompt asking the user to include uppercase characters
+var includeNumeric = confirm("Include numeric characters?"); //Confirmation prompt asking the user to include numeric characters
+var includeSpecial = confirm("Include special characters?"); //Confirmation prompt asking the user to include special characters
 
 //Asks user to confirm at least one character set option to generate a password
 if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
